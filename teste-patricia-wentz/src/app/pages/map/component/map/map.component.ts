d8b7eb1472888235188e9f57d33438aca9353481
wentzpatricia <patriciawentzm@gmail.com>
2023-02-18 20:16:02 -0300

    :sparkles: add google maps

--- teste-patricia-wentz/src/app/pages/map/component/map/map.component.ts
+++ teste-patricia-wentz/src/app/pages/map/component/map/map.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
+  apiLoaded: Observable<boolean>;
 
+  constructor(httpClient: HttpClient) {
+    this.apiLoaded = httpClient
+      .jsonp(
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyAxqLh27OBBiBBid5pX7xJiDxgsyhbv21s',
+        'callback'
+      )
+      .pipe(
+        map(() => true),
+        catchError(() => of(false))
+      );
+  }
 }
--- teste-patricia-wentz/src/app/pages/map/map.module.ts
+++ teste-patricia-wentz/src/app/pages/map/map.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapRoutingModule } from './map-routing.module';
 import { MapComponent } from './component/map/map.component';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 @NgModule({
   declarations: [MapComponent],
-  imports: [CommonModule, MapRoutingModule],
+  imports: [CommonModule, MapRoutingModule, GoogleMapsModule],
 })
 export class MapModule {}
