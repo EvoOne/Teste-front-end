e58fab6c861b649804bed7cff05fa91e4f21d6cf
wentzpatricia <patriciawentzm@gmail.com>
2023-02-19 14:14:10 -0300

    :lock: add environment variable to google maps

--- teste-patricia-wentz/src/app/pages/map/component/map/map.component.ts
+++ teste-patricia-wentz/src/app/pages/map/component/map/map.component.ts
@@ -2,20 +2,22 @@ import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
+  private URL: string = environment.API_URL;
+  private KEY: string = environment.API_KEY;
+
   apiLoaded: Observable<boolean>;
 
   constructor(httpClient: HttpClient) {
     this.apiLoaded = httpClient
-      .jsonp(
-        'https://maps.googleapis.com/maps/api/js?key=AIzaSyAxqLh27OBBiBBid5pX7xJiDxgsyhbv21s',
-        'callback'
-      )
+      .jsonp(`${this.URL}${this.KEY}`, 'callback')
       .pipe(
         map(() => true),
         catchError(() => of(false))
---
+++ teste-patricia-wentz/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://maps.googleapis.com/maps/api/js?key=',
+  API_KEY: 'AIzaSyAxqLh27OBBiBBid5pX7xJiDxgsyhbv21s',
+};
