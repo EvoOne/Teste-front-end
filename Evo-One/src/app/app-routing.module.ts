de887adafb02581201ead4a6852057b599e53eb8
KaysonN <carloskayson@hotmail.com>
2023-02-25 13:39:55 -0300

    Colocado o component de ocorrências para ser a tela de home da aplicação.

--- Evo-One/src/app/app-routing.module.ts
+++ Evo-One/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { MapsComponent } from './maps/maps.component';
 import { OccurrencesComponent } from './occurrences/occurrences.component';
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { Router, RouterModule, Routes } from "@angular/router";
 import { UsersComponent } from "./users/users.component";
 
 const routes: Routes = [
+  {path: "", component: OccurrencesComponent},
   {path: "users", component: UsersComponent},
-  {path: "occurrences", component: OccurrencesComponent},
   {path: "map", component: MapsComponent}
 ];
 
